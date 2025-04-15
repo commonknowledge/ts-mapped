8cb8316d0e49fd69a1bdfb2b05fdbd5c791dce3d
Joaquim d'Souza <joaquimds@gmail.com>
2025-04-15 11:14:47 +0200

    fix: area stats

--- src/server/stats/index.ts
+++ src/server/stats/index.ts
@@ -38,7 +38,7 @@ export const getAreaStats = async (
   const query = db
     .selectFrom("dataRecord")
     .select([
-      sql`mapped_json->'geocodeResult'->'areas'->>${areaSetCode}`.as(
+      sql`geocode_result->'areas'->>${areaSetCode}`.as(
         "areaCode",
       ),
       db.fn(operation, [sql`(json->>${column})::float`]).as("value"),
@@ -120,7 +120,7 @@ export const getMaxColumnByArea = async (
   const q = sql`
     WITH data_record_with_max_column AS (
       SELECT 
-        mapped_json->'geocodeResult'->'areas'->>${areaSetCode} as area_code,
+        geocode_result->'areas'->>${areaSetCode} as area_code,
         ${caseWhen.end()} AS max_column
       FROM data_record
       WHERE data_source_id = ${dataSourceId}
@@ -162,8 +162,8 @@ const getBoundingBoxSQL = (boundingBox: BoundingBoxInput | null) => {
         ),
         ST_SetSRID(
           ST_MakePoint(
-            (mapped_json->'geocodeResult'->'centralPoint'->>'lng')::float,
-            (mapped_json->'geocodeResult'->'centralPoint'->>'lat')::float
+            (geocode_result->'centralPoint'->>'lng')::float,
+            (geocode_result->'centralPoint'->>'lat')::float
           ),
           4326
         )
