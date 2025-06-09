7c62a2c89cf54f72283c4f168d8e0966f658720a
Joaquim d'Souza <joaquimds@gmail.com>
2025-06-09 14:43:30 +0200

    debug: temporarily disable eslint on render

--- next.config.ts
+++ next.config.ts
@@ -1,6 +1,10 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
+  eslint: {
+    // Fix build on Render
+    ignoreDuringBuilds: true
+  },
   // Fix build warning caused by graphql-yoga
   serverExternalPackages: ["@whatwg-node"],
 };
