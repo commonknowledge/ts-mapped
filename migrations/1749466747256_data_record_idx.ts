e0e80bf7d1eb1a8e14667572a4a1c8ab1144b01d
Joaquim d'Souza <joaquimds@gmail.com>
2025-06-09 13:07:43 +0200

    feat: add data_record.data_source_id index

---
+++ migrations/1749466747256_data_record_idx.ts
@@ -0,0 +1,16 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
+import { Kysely } from "kysely";
+
+export async function up(db: Kysely<any>): Promise<void> {
+  await db.schema
+    .createIndex("dataRecordDataSourceIdIndex")
+    .on("dataRecord")
+    .columns(["dataSourceId"])
+    .execute();
+}
+
+export async function down(db: Kysely<any>): Promise<void> {
+  await db.schema
+    .dropIndex("dataRecordDataSourceIdIndex")
+    .execute();
+}
