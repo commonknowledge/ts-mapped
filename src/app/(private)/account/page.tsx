4562c89422d75a82173c8e4555410e4a6ef648c9
Jack Clackett <jack@noquarter.co>
2025-08-28 09:13:34 +0200

    fix account header after pr merge

--- src/app/(private)/account/page.tsx
+++ src/app/(private)/account/page.tsx
@@ -1,13 +1,10 @@
 import PageHeader from "@/components/PageHeader";
-import { Separator } from "@/shadcn/ui/separator";
 import { ChangePasswordForm } from "./components/ChangePasswordForm";
 
 export default function Page() {
   return (
     <div>
       <PageHeader title="Account" />
-      <Separator className="my-4" />
-
       <ChangePasswordForm />
     </div>
   );
