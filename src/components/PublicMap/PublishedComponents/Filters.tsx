0db04ca789db4854d00b9e886ef6be5d601451ee
AdaBan <ada.banaszak@gmail.com>
2025-09-02 16:51:05 +0200

    logging filter fields

--- src/components/PublicMap/PublishedComponents/Filters.tsx
+++ src/components/PublicMap/PublishedComponents/Filters.tsx
@@ -1,9 +1,71 @@
+"use client";
+
+import { useContext, useEffect, useState } from "react";
+import { PublicMapColumnType } from "@/__generated__/types";
+import { PublicMapContext } from "@/components/PublicMap/PublicMapContext";
 import { Button } from "@/shadcn/ui/button";
 
+interface FilterField {
+  name: string;
+  type: PublicMapColumnType;
+  options?: string[];
+}
+
 export default function Filters() {
+  const { publicMap, activeTabId } = useContext(PublicMapContext);
+  const [filterFields, setFilterFields] = useState<FilterField[]>([]);
+
+  useEffect(() => {
+    if (!publicMap) {
+      return;
+    }
+
+    const dataSourceConfig = activeTabId
+      ? publicMap.dataSourceConfigs.find((c) => c.dataSourceId === activeTabId)
+      : publicMap.dataSourceConfigs[0];
+
+    if (dataSourceConfig) {
+      const columns = dataSourceConfig.nameColumns.concat([
+        dataSourceConfig.descriptionColumn,
+      ]);
+
+      const typedColumns = columns
+        .filter((col) => !!col)
+        .map((name) => ({
+          name,
+          type: PublicMapColumnType.String,
+        }));
+
+      for (const additionalColumn of dataSourceConfig.additionalColumns) {
+        for (const sourceColumn of additionalColumn.sourceColumns) {
+          typedColumns.push({
+            name: sourceColumn,
+            type: additionalColumn.type,
+          });
+        }
+      }
+
+      const fields = typedColumns.map((col) => {
+        if (col.type === PublicMapColumnType.CommaSeparatedList) {
+          // TODO: find options
+          return {
+            ...col,
+            options: [], // TODO
+          };
+        }
+
+        return col;
+      });
+
+      setFilterFields(fields);
+    }
+  }, [publicMap, activeTabId]);
+
   return (
     <div className="my-4 px-4">
-      <Button variant="outline">Log filters</Button>
+      <Button variant="outline" onClick={() => console.log(filterFields)}>
+        Log filters
+      </Button>
     </div>
   );
 }
