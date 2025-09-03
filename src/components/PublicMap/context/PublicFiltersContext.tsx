d08e0b7d84ea918240fee7ef1538e84278bf48d2
AdaBan <ada.banaszak@gmail.com>
2025-09-03 13:34:24 +0200

    Typing in filters context

--- src/components/PublicMap/context/PublicFiltersContext.tsx
+++ src/components/PublicMap/context/PublicFiltersContext.tsx
@@ -1,8 +1,11 @@
 import { createContext } from "react";
+import type { PublicFiltersFormValue } from "@/types";
 
 export const PublicFiltersContext = createContext<{
   publicFilters: object[];
-  setPublicFilters: React.Dispatch<React.SetStateAction<object[]>>;
+  setPublicFilters: React.Dispatch<
+    React.SetStateAction<PublicFiltersFormValue[]>
+  >;
 }>({
   publicFilters: [],
   setPublicFilters: () => [],
