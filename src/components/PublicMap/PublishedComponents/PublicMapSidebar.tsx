bbd126f95e6ee50d0938f1f0498e92433cb53f79
AdaBan <ada.banaszak@gmail.com>
2025-09-09 10:51:12 +0200

    Fixing heading alignement

--- src/components/PublicMap/PublishedComponents/PublicMapSidebar.tsx
+++ src/components/PublicMap/PublishedComponents/PublicMapSidebar.tsx
@@ -42,7 +42,7 @@ export default function PublicMapSidebar() {
             style={{ backgroundColor: activeColourScheme.muted }}
             className="p-4 flex flex-col gap-6"
           >
-            <div className="flex flex-col items-center justify-between gap-2">
+            <div className="flex flex-col w-full items-start justify-between gap-2">
               <div className="flex items-center gap-2">
                 <MapIcon
                   className="w-10 h-10 shrink-0"
