1f188551c31a3de2a8ebf45bb9c5a39e8c633d68
AdaBan <ada.banaszak@gmail.com>
2025-09-11 11:47:14 +0200

    Account page layout

--- src/app/(private)/account/page.tsx
+++ src/app/(private)/account/page.tsx
@@ -1,11 +1,21 @@
 import PageHeader from "@/components/PageHeader";
-import { ChangePasswordForm } from "./components/ChangePasswordForm";
+// import { ChangePasswordForm } from "./components/ChangePasswordForm";
 
 export default function Page() {
   return (
     <div>
-      <PageHeader title="Account" />
-      <ChangePasswordForm />
+      <PageHeader title="Settings" />
+      <div className="grid grid-cols-2 gap-8">
+        <div className="">
+          <h2 className="mb-6 / text-xl font-medium">Your profile</h2>
+          {/* <ChangePasswordForm /> */}
+        </div>
+
+        <div className="">
+           <h2 className="mb-6 / text-xl font-medium">Organisation settings</h2>
+
+        </div>
+      </div>
     </div>
   );
 }
