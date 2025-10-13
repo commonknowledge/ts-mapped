2092c369f3c8b015a51ff05d7c22b1361b6db631
Jack Clackett <jack@noquarter.co>
2025-10-13 14:21:07 +0200

    do we need refetch?

--- src/app/map/[id]/hooks/useMapQuery.ts
+++ src/app/map/[id]/hooks/useMapQuery.ts
@@ -6,7 +6,7 @@ export function useMapQuery(mapId: string | null | undefined) {
   return useQuery(
     trpc.map.byId.queryOptions(
       { mapId: mapId || "" },
-      { enabled: Boolean(mapId), refetchOnMount: true },
+      { enabled: Boolean(mapId) },
     ),
   );
 }
