679ecd644f33462d947f27e2722223f96814453e
Jan Baykara <jan@commonknowledge.coop>
2025-10-21 23:49:23 +0100

    Lint fix

--- tests/feature/geojsonAPI.test.ts
+++ tests/feature/geojsonAPI.test.ts
@@ -1,5 +1,5 @@
 import { v4 as uuidv4 } from "uuid";
-import { afterAll, beforeAll, describe, expect, it } from "vitest";
+import { beforeAll, describe, expect, it } from "vitest";
 import {
   ColumnType,
   DataSourceRecordType,
