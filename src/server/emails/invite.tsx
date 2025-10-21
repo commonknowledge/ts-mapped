51515a8c7da23fdd24f8cedbb0682b347b580d07
Joaquim d'Souza <joaquimds@gmail.com>
2025-10-21 17:02:19 +0200

    feat: add new invite email copy

--- src/server/emails/invite.tsx
+++ src/server/emails/invite.tsx
@@ -29,11 +29,16 @@ export default function Invite({ token = "123" }: { token: string }) {
         </Section>
 
         <Text className="text-[14px] text-black font-sans leading-[24px]">
-          Hello!
+          Hello,
         </Text>
 
         <Text className="text-[14px] text-black font-sans leading-[24px]">
-          You have been invited to Mapped. You can confirm your invite here:
+          Thanks for your patience while we’ve been building the newest version
+          of Mapped. We’re happy to share that it is now ready to use!
+        </Text>
+
+        <Text className="text-[14px] text-black font-sans leading-[24px]">
+          You can confirm your invite here:
         </Text>
         <Section className="mt-[32px] mb-[32px] text-center">
           <Button
@@ -43,6 +48,32 @@ export default function Invite({ token = "123" }: { token: string }) {
             Confirm invite
           </Button>
         </Section>
+
+        <Text className="text-[14px] text-black font-sans leading-[24px]">
+          We’ve been rigorously testing the platform internally, but as with any
+          software there will sometimes be bugs. If something doesn’t work for
+          you, you can report a bug using the icon in the bottom right of the
+          platform.
+        </Text>
+
+        <Text className="text-[14px] text-black font-sans leading-[24px]">
+          Mapped is built with movements, not just for them. If you use it, we’d
+          love to hear what works for your organising and what doesn’t — just
+          email{" "}
+          <a href="mailto:mapped@commonknowledge.coop">
+            mapped@commonknowledge.coop
+          </a>
+          . Your feedback will shape what we build next.
+        </Text>
+
+        <Text className="text-[14px] text-black font-sans leading-[24px]">
+          We fund the ongoing development of Mapped through a mix of grants,
+          feature commissions, individual donations and our own surplus income.
+          Mapped will always be free for grassroots organisers, but if you’re in
+          the position to support our work with a donation, it would go a long
+          way to making this work sustainable.
+        </Text>
+
         <Hr className="mx-0 my-[26px] w-full border border-[#eaeaea] border-solid" />
         <Text className="text-[#666666] text-[12px] font-sans leading-[24px]">
           If you did not request an invite, please ignore this email.
