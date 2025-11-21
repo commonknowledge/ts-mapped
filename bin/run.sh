49b15737fb2cbc7dc8d265ce2fd96c6e0f4d83b1
ev <4164774+ev-sc@users.noreply.github.com>
2025-11-21 06:49:53 +0100

    Better run script

--- bin/run.sh
+++ bin/run.sh
@@ -11,5 +11,7 @@ pretty_print() {
 pretty_print "Starting the development server..."
 pretty_print "View the site at https://localhost:3000\nLog in with username 'hello@commonknowledge.coop' and password '1234'"
 
+docker compose up -d
+npm run migrate
 npm i
 npm run dev
