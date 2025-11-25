8e4a2089acf5e3ce39be02d94e0acb664f99ba64
Joaquim d'Souza <joaquimds@gmail.com>
2025-11-25 16:51:38 +0100

    refactor: replace Airtable source with CSV source in feature test (to protect API limits)

--- tests/feature/importDataSource.test.ts
+++ tests/feature/importDataSource.test.ts
@@ -14,26 +14,22 @@ import {
 } from "@/server/repositories/DataSource";
 import { upsertOrganisation } from "@/server/repositories/Organisation";
 
-const credentials = inject("credentials");
-
 describe("importDataSource tests", () => {
-  test("importDataSource imports John Lennon record from Airtable", async () => {
+  test("importDataSource imports records from CSV", async () => {
     // 1. Create test organisation
     const org = await upsertOrganisation({
       name: "Test Import Org",
     });
 
-    // 2. Create test data source with Airtable credentials
+    // 2. Create test data source with CSV file
     const dataSource = await createDataSource({
-      name: "Test Import Airtable Source",
+      name: "Test Import CSV Source",
       autoEnrich: false,
       autoImport: false,
       recordType: DataSourceRecordType.Data,
       config: {
-        type: DataSourceType.Airtable,
-        apiKey: credentials.airtable.apiKey,
-        baseId: credentials.airtable.baseId,
-        tableId: credentials.airtable.tableId,
+        type: DataSourceType.CSV,
+        url: "file://tests/resources/members.csv",
       },
       columnDefs: [],
       columnRoles: { nameColumns: [] },
@@ -54,7 +50,7 @@ describe("importDataSource tests", () => {
     const stream = streamDataRecordsByDataSource(
       dataSource.id,
       { type: FilterType.MULTI },
-      "",
+      ""
     );
     const records = [];
     for await (const record of stream) {
@@ -74,36 +70,10 @@ describe("importDataSource tests", () => {
       records.map((r) => ({
         ...r,
         json: { Name: r.json.Name, Postcode: r.json.Postcode },
-      })),
+      }))
     ).toEqual([
       {
-        externalId: "recHSNLI2dfwSoo8U",
-        geocodePoint: { lat: 51.651882912, lng: -0.090255219 },
-        geocodeResult: {
-          areas: { PC: "EN26PJ" },
-          centralPoint: { lat: 51.651882912, lng: -0.090255219 },
-          samplePoint: { lat: 51.65194185, lng: -0.090364789 },
-        },
-        json: {
-          Name: "Ringo",
-          Postcode: "EN2 6PJ",
-        },
-      },
-      {
-        externalId: "reckBm7wJxGA5R0bV",
-        geocodePoint: { lat: 51.559292453, lng: -0.120000759 },
-        geocodeResult: {
-          areas: { PC: "N76AS" },
-          centralPoint: { lat: 51.559292453, lng: -0.120000759 },
-          samplePoint: { lat: 51.5592981, lng: -0.119958004 },
-        },
-        json: {
-          Name: "Paul",
-          Postcode: "N76AS",
-        },
-      },
-      {
-        externalId: "recv4q88wppg6xSGw",
+        externalId: "1",
         geocodePoint: { lat: 51.156594417, lng: 0.259700363 },
         geocodeResult: {
           areas: { PC: "TN40PP" },
@@ -116,7 +86,20 @@ describe("importDataSource tests", () => {
         },
       },
       {
-        externalId: "recyazhCjEnpZMqiy",
+        externalId: "2",
+        geocodePoint: { lat: 51.559292453, lng: -0.120000759 },
+        geocodeResult: {
+          areas: { PC: "N76AS" },
+          centralPoint: { lat: 51.559292453, lng: -0.120000759 },
+          samplePoint: { lat: 51.5592981, lng: -0.119958004 },
+        },
+        json: {
+          Name: "Paul",
+          Postcode: "N76AS",
+        },
+      },
+      {
+        externalId: "3",
         geocodePoint: { lat: 55.870966132, lng: -4.299308293 },
         geocodeResult: {
           areas: { PC: "G115RD" },
@@ -126,6 +109,19 @@ describe("importDataSource tests", () => {
         json: {
           Name: "George",
           Postcode: "G115RD",
+        },
+      },
+      {
+        externalId: "4",
+        geocodePoint: { lat: 51.651882912, lng: -0.090255219 },
+        geocodeResult: {
+          areas: { PC: "EN26PJ" },
+          centralPoint: { lat: 51.651882912, lng: -0.090255219 },
+          samplePoint: { lat: 51.65194185, lng: -0.090364789 },
+        },
+        json: {
+          Name: "Ringo",
+          Postcode: "EN2 6PJ",
         },
       },
     ]);
