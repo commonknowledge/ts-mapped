2f5bd463abcc88cf6d2aebb2f80511a9ff286051
Joaquim d'Souza <joaquimds@gmail.com>
2025-12-02 16:24:17 +0100

    fix: remove duplicate function

--- src/app/map/[id]/sources.ts
+++ src/app/map/[id]/sources.ts
@@ -1,5 +1,5 @@
-import { AreaSetCode, AreaSetGroupCode } from "@/server/models/AreaSet";
-import type { GeocodingConfig } from "@/server/models/DataSource";
+import { AreaSetCode } from "@/server/models/AreaSet";
+import type { AreaSetGroupCode } from "@/server/models/AreaSet";
 
 export interface ChoroplethLayerConfig {
   areaSetCode: AreaSetCode;
@@ -107,45 +107,4 @@ export const getChoroplethLayerConfig = (
     }
   }
   return CHOROPLETH_LAYER_CONFIGS["WMC24"][0];
-};
-
-// Return the area set groups that are valid as visualisation options
-// for a given data source geocoding config
-export const getValidAreaSetGroupCodes = (
-  dataSourceGeocodingConfig: GeocodingConfig | null | undefined,
-): AreaSetGroupCode[] => {
-  if (!dataSourceGeocodingConfig) {
-    return [];
-  }
-
-  const areaSetCode =
-    "areaSetCode" in dataSourceGeocodingConfig
-      ? dataSourceGeocodingConfig.areaSetCode
-      : null;
-
-  if (areaSetCode) {
-    // Get a list of area sets that are smaller or equal in size
-    // to the data source area set
-    const dataSourceAreaSize = AREA_SET_SIZES[areaSetCode];
-    const validAreaSets = Object.keys(AREA_SET_SIZES).filter(
-      (code) => AREA_SET_SIZES[code as AreaSetCode] >= dataSourceAreaSize,
-    );
-
-    // Get the associated group for each valid area set
-    // Uses the above CHOROPLETH_LAYER_CONFIGS to match area set to group
-    const validAreaGroups = new Set<AreaSetGroupCode>();
-    for (const areaSetCode of validAreaSets) {
-      for (const areaSetGroupCode of Object.keys(CHOROPLETH_LAYER_CONFIGS)) {
-        const sources =
-          CHOROPLETH_LAYER_CONFIGS[areaSetGroupCode as AreaSetGroupCode];
-        if (sources.some((s) => s.areaSetCode === areaSetCode)) {
-          validAreaGroups.add(areaSetGroupCode as AreaSetGroupCode);
-        }
-      }
-    }
-    return validAreaGroups.values().toArray();
-  }
-
-  // Default to all options
-  return Object.values(AreaSetGroupCode);
 };
