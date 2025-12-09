2528c33c58dd189d9e6164db1849b88581204ddc
Joaquim d'Souza <joaquimds@gmail.com>
2025-12-09 18:26:37 +0100

    chore: lint

--- src/app/map/[id]/components/MapStyleSelector.tsx
+++ src/app/map/[id]/components/MapStyleSelector.tsx
@@ -1,7 +1,7 @@
 import { TooltipTrigger } from "@radix-ui/react-tooltip";
 import { HexagonIcon, Layers, MapIcon, X } from "lucide-react";
 import Image from "next/image";
-import { useContext, useState, memo } from "react";
+import { memo, useContext, useState } from "react";
 import { useMapViews } from "@/app/map/[id]/hooks/useMapViews";
 import FormFieldWrapper from "@/components/forms/FormFieldWrapper";
 import { NULL_UUID } from "@/constants";
